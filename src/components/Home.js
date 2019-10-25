90f4c76096738bef2c925dc24b27eb342375185d
sh183694@gmail.com <sh183694@gamil.com>
2019-10-26 06:12:36 +0900

    Add Router Module

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <Card>
+        <CardContent>포트폴리오 페이지 제작하기</CardContent>
+      </Card>
+    );
+  }
+}
+
+export default Home;
